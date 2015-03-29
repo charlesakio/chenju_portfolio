ade0dd1754723bb1cc289c973e03896853827d41
Julio Tain Sueiras <juliosueiras@gmail.com>
2015-03-29 17:33:03 -0400

    redone the migrations

--- myapp/migrations/0001_initial.py
+++ myapp/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django_markdown.models
 
 
 class Migration(migrations.Migration):
@@ -15,7 +16,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
                 ('title', models.CharField(max_length=200)),
-                ('body', models.TextField()),
+                ('body', django_markdown.models.MarkdownField()),
                 ('slug', models.SlugField(unique=True, max_length=200)),
                 ('publish', models.BooleanField(default=True)),
                 ('created', models.DateTimeField(auto_now_add=True)),
